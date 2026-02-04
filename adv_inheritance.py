75f6e52c2ef7da16d42b8a14fad7c4b87073b4f2
Kumkum-Mishra <kumkummishra892004@gmail.com>
2026-02-04 13:56:22 +0530

    new changes

--- adv_inheritance.py
+++ adv_inheritance.py
@@ -1,23 +1,22 @@
 # Single or Basic Inheritance
 
 # Base class
-# class Parent:
-#     def __init__(self, name):
-#         self.name = name
+class Parent:
+    def __init__(self, name):
+        self.name = name
 
-#     def greet(self):
-#         print(f"Hello, my name is {self.name}.")
+    def greet(self):
+        print(f"Hello, my name is {self.name}.")
 
 # # Derived class
-# class Child(Parent):
-
-#     def play(self):
-#         print(f"{self.name} is playing.")
+class Child(Parent):
+    def play(self):
+        print(f"{self.name} is playing.")
 
 # # Create an instance of Child
-# child = Child("Alice")
-# child.greet()  # Output: Hello, my name is Alice.
-# child.play()   # Output: Alice is playing.
+child = Child("Alice")
+child.greet()  # Output: Hello, my name is Alice.
+child.play()   # Output: Alice is playing.
 
 # ------------------------------------------------------------
 
